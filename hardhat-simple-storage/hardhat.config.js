c84ae407a9d62b71ffd47070a039753afab8dafb
shisukeUrahara <abhisheksagu@gmail.com>
2022-06-21 19:39:39 +0530

    hardhat-simple-storage: Added tests and gas reporter

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCFAP_API_KEY = process.env.COINMARKETCFAP_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -33,5 +35,13 @@ module.exports = {
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCFAP_API_KEY,
+        token: "MATIC",
     },
 }
