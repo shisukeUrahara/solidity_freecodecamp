3144d1ae956e7a4f6fabc383de84e0bd829d9fcf
shisukeUrahara <abhisheksagu@gmail.com>
2022-06-24 11:53:24 +0530

    hardhat-simple-storage: Resolved typo errors

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -18,7 +18,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const COINMARKETCFAP_API_KEY = process.env.COINMARKETCFAP_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -42,7 +42,7 @@ module.exports = {
         outputFile: "gas-report.txt",
         noColors: true,
         currency: "USD",
-        coinmarketcap: COINMARKETCFAP_API_KEY,
+        coinmarketcap: COINMARKETCAP_API_KEY,
         token: "MATIC",
     },
 }
