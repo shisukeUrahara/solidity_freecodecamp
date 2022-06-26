0055963b21935cb06b17995fb7aeb755aaddab66
shisukeUrahara <abhisheksagu@gmail.com>
2022-06-26 19:04:10 +0530

    hardhat-smart-contract-lottery: initial commit

---
+++ hardhat-smart-contract-lottery/contracts/Lottery.sol
@@ -0,0 +1,38 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+error Lottery_NotEnoughEthEntered();
+
+contract Lottery {
+    //   state variables
+    uint256 private immutable i_entryFees;
+    address payable[] private s_players;
+
+    //  events
+    event LotteryEnter(address indexed playerAddress);
+
+    //  modifiers
+
+    //  constructor
+    constructor(uint256 entryFees) {
+        i_entryFees = entryFees;
+    }
+
+    //  functions
+    function enterLottery() public payable {
+        if (msg.value < i_entryFees) {
+            revert Lottery_NotEnoughEthEntered();
+        }
+
+        s_players.push(payable(msg.sender));
+        //  emitting events
+        emit LotteryEnter(msg.sender);
+    }
+
+    function getEntryFees() public view returns (uint256) {
+        return i_entryFees;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
