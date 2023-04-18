a91f038ecdc706e5d8e1530f1dcf6a2fd3588749
ShisukeUrahara <abhisheksagu@gmail.com>
2023-04-18 21:15:47 +0530

    reset repo

--- hardhat-simple-storage/tasks/block-number.js
+++
@@ -1,8 +0,0 @@
-const { task } = require("hardhat/config")
-
-task("block-number", "Prints latest block number").setAction(
-    async (taskArgs, hre) => {
-        const blockNumber = await hre.ethers.provider.getBlockNumber()
-        console.log("**@ Current block number is , ", blockNumber)
-    }
-)
